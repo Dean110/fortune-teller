c7fceaf834f25ebc80d6df630e317cf0fd052091
BenWilliams <deanpbr@gmail.com>
2018-01-19 10:22:09 -0500

    Add readme and progress on main project.

--- src/FortuneTeller.java
+++ src/FortuneTeller.java
@@ -1,8 +1,24 @@
+import java.util.Scanner;
 
 public class FortuneTeller {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Scanner input = new Scanner (System.in);
+		
+		//Program startup text.
+		
+		System.out.println("Welcome to WCCI Industries \'Super Awesome Fortune Teller\'");
+
+		//Initialize and fill user data variables.
+		
+		System.out.println("To start I'll need your first name:");
+		String userFirstName = input.nextLine();
+		System.out.println("Thank you " + userFirstName + ", next I need your last name:");
+		String userLastName = input.nextLine();
+		System.out.println(userLastName);
+		
+		
 
 	}
 
